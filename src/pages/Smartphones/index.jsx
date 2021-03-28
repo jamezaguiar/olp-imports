5c98549491a4f89f5bff104600eacff274a80ef9
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-28 14:45:49 -0300

    Getting data from API

--- src/pages/Smartphones/index.jsx
+++ src/pages/Smartphones/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
 
 import { Container, Card, ImageContainer } from './styles';
 
 export default function Smartphones() {
+  const [smartphones, setSmartphones] = useState([]);
+
+  useEffect(() => {
+    api.get('/phones').then((response) => setSmartphones(response.data));
+  }, []);
+
   return (
     <Container>
-      <Card>
-        <ImageContainer>
-          <img
-            src="https://img.ibxk.com.br/2019/04/10/10115741942666.jpg"
-            alt="Galaxy A10"
-          />
-        </ImageContainer>
-        <h4>Samsung Galaxy A10</h4>
-        <span>Samsung</span>
-        <p>R$1000</p>
-      </Card>
+      {smartphones.map((smartphone) => (
+        <Card key={smartphone._id}>
+          <ImageContainer>
+            <img src={smartphone.image_url} alt={smartphone.name} />
+          </ImageContainer>
+          <h4>{smartphone.name}</h4>
+          <span>{smartphone.brand}</span>
+          <p>R${smartphone.price}</p>
+        </Card>
+      ))}
     </Container>
   );
 }
