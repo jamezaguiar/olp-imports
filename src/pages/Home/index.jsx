445ff0e3afea123a09fa656b63da5f289fd5de99
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-28 15:02:12 -0300

    Home page finalized

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import api from '../../services/api';
+
+import { Container, ImageContainer, Card } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('/').then((response) => setProducts(response.data));
+  }, []);
+
   return (
     <Container>
-      <h1>Home</h1>
+      {products.map((product) => (
+        <Card key={product._id}>
+          <ImageContainer>
+            <img src={product.image_url} alt={product.name} />
+          </ImageContainer>
+          <h4>{product.name}</h4>
+          <span>{product.brand}</span>
+          <p>R${product.price}</p>
+        </Card>
+      ))}
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,49 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 20px;
+
+  padding: 16px;
+`;
+
+export const Card = styled.div`
+  background-color: #fff;
+  padding: 16px;
+
+  border-radius: 8px;
+
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+
+  img {
+    height: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  h4 {
+    font-weight: 600;
+    font-size: 24px;
+  }
+
+  span {
+    color: #aaa;
+  }
+
+  p {
+    color: #43aa8b;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  /* height: 50%; */
+  padding-top: 56.25%; /* 16:9 */
+
+  overflow: hidden;
+  position: relative;
+
+  margin-bottom: 16px;
+`;
