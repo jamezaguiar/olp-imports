74a55d0ba29ef18ed8b900e23105b5f3cd72f58d
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-28 15:06:48 -0300

    Others and Consoles pages finalized

--- src/pages/Consoles/index.jsx
+++ src/pages/Consoles/index.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import { Container, Card, ImageContainer } from './styles';
 
 export default function Consoles() {
-  return <h1>Consoles</h1>;
+  const { params } = useRouteMatch(); // { brand: string }
+  const [consoles, setConsoles] = useState([]);
+
+  useEffect(() => {
+    if (params.brand) {
+      api
+        .get(`/consoles/${params.brand}`)
+        .then((response) => setConsoles(response.data));
+    } else {
+      api.get('/consoles').then((response) => setConsoles(response.data));
+    }
+  }, [params.brand]);
+
+  return (
+    <Container>
+      {consoles.map((console) => (
+        <Card key={console._id}>
+          <ImageContainer>
+            <img src={console.image_url} alt={console.name} />
+          </ImageContainer>
+          <h4>{console.name}</h4>
+          <span>{console.brand}</span>
+          <p>R${console.price}</p>
+        </Card>
+      ))}
+    </Container>
+  );
 }
--- src/pages/Others/index.jsx
+++ src/pages/Others/index.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
+
+import { Container, Card, ImageContainer } from './styles';
 
 export default function Others() {
-  return <h1>Others</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('/others').then((response) => setProducts(response.data));
+  }, []);
+
+  return (
+    <Container>
+      {products.map((product) => (
+        <Card key={product._id}>
+          <ImageContainer>
+            <img src={product.image_url} alt={product.name} />
+          </ImageContainer>
+          <h4>{product.name}</h4>
+          <span>{product.brand}</span>
+          <p>R${product.price}</p>
+        </Card>
+      ))}
+    </Container>
+  );
 }
