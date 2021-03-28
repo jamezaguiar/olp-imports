2494c538f3848a771a90ebdd942f788c55326161
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-28 13:11:02 -0300

    Smartphone page grid finalized

--- src/pages/Smartphones/index.jsx
+++ src/pages/Smartphones/index.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+import { Container, Card, ImageContainer } from './styles';
+
 export default function Smartphones() {
-  return <h1>Smartphones</h1>;
+  return (
+    <Container>
+      <Card>
+        <ImageContainer>
+          <img
+            src="https://img.ibxk.com.br/2019/04/10/10115741942666.jpg"
+            alt="Galaxy A10"
+          />
+        </ImageContainer>
+        <h4>Samsung Galaxy A10</h4>
+        <span>Samsung</span>
+        <p>R$1000</p>
+      </Card>
+    </Container>
+  );
 }
